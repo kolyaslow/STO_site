03e95f9595f8cfb77cce1b28da21d52c312ac6a5
79146 <pysenkon@gmail.com>
2023-06-07 16:38:18 +0700

    добавлино условие неккоректной отправки сообщения в телеграмм

--- main.py
+++ main.py
@@ -6,28 +6,35 @@ from keys_and_id import SECRET_KEY
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+name_error = 'Имя должно содержать только русские символы'
+len_vin_number_error = 'Vin номер должен содержать не более 17 символов'
+vin_number_error = 'Не корректный Vin номер'
+len_phone_number_error = 'Номер телефона должен содержать не более 11 символов'
+phone_number_error = 'Недопустимые символы в номере телефона'
+sent_successfully = 'Форма отправлена, ждите звонка'
+
 def data_validity_check(form):
     #проверка имени на содержание не русских символов
     for char in form['name']:
         if  char in ascii_letters + punctuation + whitespace:
-            return 'Имя должно содержать только русские символы'
+            return name_error
 
     if len(form['vin_number']) > 17:
-        return 'Vin номер должен содержать не более 17 символов'
+        return len_vin_number_error
 
     # проверка на содержание спец сиволов
     for char in form['vin_number']:
         if char in punctuation:
-            return 'Не корректный Vin номер'
+            return vin_number_error
 
     #проверка номера талефона
-    if len(form['phone_number']) > 17:
-        return 'Номер телефона должен содержать не более 11 символов'
+    if len(form['phone_number']) > 11:
+        return len_phone_number_error
 
     if not form['phone_number'].isdigit():
-        return 'Недопустимые символы в номере телефона'
+        return phone_number_error
 
-    return 'Форма отправлена, ждите звонка'
+    return sent_successfully
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -36,17 +43,21 @@ def main_page():
     if request.method == 'POST':
         form = request.form
         error_data_or_ok = data_validity_check(form)
-        if error_data_or_ok == 'Успешная отправка':
+        if error_data_or_ok == sent_successfully:
 
             message = f"Имя: {form['name']} \n" \
                         f"Номер телефона: {form['phone_number']} \n" \
                         f"VIN номер: {form['vin_number']} \n" \
                         f"Услуга: {form['service']}"
-            send_telegram_message(message)
-            flash(error_data_or_ok, category='sent_successfully')
+
+            if send_telegram_message(message):
+                print("Я тут")
+                flash(error_data_or_ok, category='sent_successfully')
+            else:
+                flash('Ошибка отправки сообщения', category='error')
         else:
             #ормирование подсказок отправки формы
-            flash(error_data_or_ok, category='error')
+            flash('*' + error_data_or_ok, category='error')
 
     return render_template('index.html')
 
