30cb56c5e66aaf5af6e0a34c937eb5b89cbd1475
79146 <pysenkon@gmail.com>
2023-06-07 23:25:15 +0700

    формирование формы при помощи класса формы Flask

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired
 
 
 class RequestForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired()])
-    phone_number = StringField('Phone', validators=[InputRequired()])
-    vin_number = StringField('VIN Number', validators=[InputRequired()])
-    service = StringField('Service', validators=[InputRequired()])
+    name = StringField('Name', validators=[InputRequired()], render_kw={'class_': 'inp_1'})
+    phone_number = StringField('Phone', validators=[InputRequired()], render_kw={'class_': 'inp_2'})
+    vin_number = StringField('VIN Number', validators=[InputRequired()], render_kw={'class_': 'inp_3'})
+    service = StringField('Service', validators=[InputRequired()], render_kw={'class_': 'inp_4'})
 
