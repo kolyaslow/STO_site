7858edb08bc4291b0777f19d9810b0c1e333673e
79146 <pysenkon@gmail.com>
2023-06-07 14:05:02 +0700

    добавлена функция data_validity_check

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
-from flask import Flask, render_template, request
-from .telegram.send_telegram_message import send_telegram_message
+from flask import Flask, render_template, request, flash
+from telegram.send_telegram_message import send_telegram_message
+from string import ascii_letters, punctuation, whitespace
 
 app = Flask(__name__)
 
 
+def data_validity_check(form):
+    #проверка имени на содержание не русских символов
+    for char in form['name']:
+        if  char in ascii_letters + punctuation + whitespace:
+            return 'Имя должно содержать только русские символы'
+
+    if len(form['vin_number']) > 17:
+        return 'Vin номер должен содержать не более 17 символов'
+
+    # проверка на содержание спец сиволов
+    for char in form['vin_number']:
+        if char in punctuation:
+            return 'Не корректный Vin номер'
+
+    #проверка номера талефона
+    if len(form['phone_number']) > 17:
+        return 'Номер телефона должен содержать не более 11 символов'
+
+    if not form['phone_number'].isdigit():
+        return 'Недопустимые символы в номере телефона'
+
+    return 'Успешная отправка'
+
+
 @app.route('/', methods=['POST', 'GET'])
 def main_page():
+
     if request.method == 'POST':
         form = request.form
-        message = f"Имя: {form['name']} \n" \
-                    f"Номер телефона: {form['phone_number']} \n" \
-                    f"VIN номер: {form['vin_number']} \n" \
-                    f"Услуга: {form['service']}"
-        print(message)
-        send_telegram_message(message)
+        error_data_or_ok = data_validity_check(form)
+        if error_data_or_ok == 'Успешная отправка':
+
+            message = f"Имя: {form['name']} \n" \
+                        f"Номер телефона: {form['phone_number']} \n" \
+                        f"VIN номер: {form['vin_number']} \n" \
+                        f"Услуга: {form['service']}"
+            send_telegram_message(message)
+        else:
+            #ормирование подсказок отправки формы
+            flash('error_data_or_ok')
+
     return render_template('index.html')
 
 
